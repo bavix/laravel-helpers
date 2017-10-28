efaa2324439262167d1736a6e99408f09f3906e0
Babichev Maxim <info@babichev.net>
2017-10-28 14:59:43 +0300

    add schema

---
+++ src/Illuminate/Support/Facades/Schema.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bavix\Illuminate\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static \Illuminate\Database\Schema\Builder create(string $table, \Closure $callback)
+ * @method static \Illuminate\Database\Schema\Builder drop(string $table)
+ * @method static \Illuminate\Database\Schema\Builder dropIfExists(string $table)
+ * @method static \Illuminate\Database\Schema\Builder table(string $table, \Closure $callback)
+ *
+ * @see \Illuminate\Database\Schema\Builder
+ */
+class Schema extends \Illuminate\Support\Facades\Schema
+{
+
+}
+
