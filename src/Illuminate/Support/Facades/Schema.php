e13b66758c3bf8152e4628d3b51f3692a1583876
Babichev Maxim <info@babichev.net>
2017-10-28 15:06:03 +0300

    update blueprint

--- src/Illuminate/Support/Facades/Schema.php
+++ src/Illuminate/Support/Facades/Schema.php
@@ -3,6 +3,7 @@
 namespace Bavix\Illuminate\Support\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Bavix\Illuminate\Database\Schema\Blueprint;
 
 /**
  * @method static \Illuminate\Database\Schema\Builder create(string $table, \Closure $callback)
@@ -12,8 +13,49 @@ use Illuminate\Support\Facades\Facade;
  *
  * @see \Illuminate\Database\Schema\Builder
  */
-class Schema extends \Illuminate\Support\Facades\Schema
+class Schema extends Facade
 {
 
-}
+    /**
+     * @param string|null $name
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected static function schemaBuilder($name = null)
+    {
+        /**
+         * @var $db \Illuminate\Database\DatabaseManager
+         */
+        $db = static::$app['db'];
 
+        $builder = $db->connection($name)->getSchemaBuilder();
+
+        $builder->blueprintResolver(function ($table, $callback) {
+            return new Blueprint($table, $callback);
+        });
+
+        return $builder;
+    }
+
+    /**
+     * Get a schema builder instance for a connection.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    public static function connection($name)
+    {
+        return static::schemaBuilder($name);
+    }
+
+    /**
+     * Get a schema builder instance for the default connection.
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected static function getFacadeAccessor()
+    {
+        return static::schemaBuilder();
+    }
+
+}
