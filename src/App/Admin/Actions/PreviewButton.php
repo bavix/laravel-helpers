74ce4976ac1953f357d9526e0efdd98aab400106
Babichev Maxim <info@babichev.net>
2017-10-28 18:41:51 +0300

    update preview button

--- src/App/Admin/Actions/PreviewButton.php
+++ src/App/Admin/Actions/PreviewButton.php
@@ -5,4 +5,8 @@ namespace Bavix\App\Admin\Actions;
 class PreviewButton extends DefaultButton
 {
     protected $icon = 'fa-eye';
+
+    protected $attributes = [
+        'taget' => '_blank'
+    ];
 }
